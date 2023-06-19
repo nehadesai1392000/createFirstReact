04ed1b1960b2a565400f6ed35619fa1961d0e335
nehadesai1392000 <nehadesai1392000@gmail.com>
2023-06-19 11:01:05 +0530

    neha

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import './App.css';
+import Navbar from './componate/Navbar';
+import TextForm from './componate/TextForm';
+import About from './componate/About';
+import { useState } from 'react';
+import Alert from './componate/Alert';
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert("");
+    }, 3000);
+  }
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#9BBBBA";
+      showAlert("Dark Mode has been enabled", "success")
+    } else {
+
+      setMode("light")
+      document.body.style.backgroundColor = "white";
+      showAlert("light Mode has been enabled", "success")
+    }
+  }
+
+  const toggleSecondaryMode = () => {
+    if (mode === "light" || mode === "dark" || mode === "Success" || mode === "Danger" || mode === "Warning") {
+      setMode("secondary");
+      document.body.style.background = "#6C757D"
+      showAlert("Secondary Mode has been enabled", "success")
+    }
+  }
+
+  const toggleSuccessMode = () => {
+    if (mode === "light" || mode === "dark" || mode === "secondary" || mode === "Danger" || mode === "Warning") {
+      setMode("Success");
+      document.body.style.background = "#198754"
+      showAlert("Success Mode has been enabled", "success")
+    }
+  }
+
+  const toggleDangerMode = () => {
+    if (mode === "light" || mode === "dark" || mode === "secondary" || mode === "Success" || mode === "Warning") {
+      setMode("Danger");
+      document.body.style.background = "red"
+      showAlert("Danger Mode has been enabled", "success")
+    }
+  }
+
+  const toggleWarningMode = () => {
+    if (mode === "light" || mode === "dark" || mode === "secondary" || mode === "Danger" || mode === "Success") {
+      setMode("Warning");
+      document.body.style.background = "#FFC107"
+      showAlert("Warning Mode has been enabled", "success")
+    }
+  }
+
+  return (
+    <>
+
+     <BrowserRouter>
+      <Navbar mode={mode} toggleMode={toggleMode} toggleSecondaryMode={toggleSecondaryMode} toggleSuccessMode={toggleSuccessMode} toggleDangerMode={toggleDangerMode} toggleWarningMode={toggleWarningMode} />
+      <Alert alert={alert} />
+      <Routes>
+        <Route path="/" element={< TextForm heading={"Enter the text to analyze below"} mode={mode} showAlert={showAlert}/>} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/componate/TextForm.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+
+const TextForm = (props) => {
+  console.log("props",props.mode)
+
+  const [text, setText] = useState(" ")
+
+  const handelUpClick = () => {
+    const newText = text.toUpperCase();
+    setText(newText)
+      props.showAlert("convert to UpperCase !", "Success")
+  }
+
+  const handelLoClick = () => {
+    const newText = text.toLowerCase();
+    setText(newText)
+    props.showAlert("convert to LowerCase !", "Success")
+  }
+
+  const handelClearClick = () => {
+    const newText = '';
+    setText(newText)
+    props.showAlert("Text Cleared !", "Success")
+  }
+
+  const handelCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Copy To ClipBoard !", "Success")
+  }
+
+const handelExtraSpaces = () => {
+  let newText = text.split(/[ ]+/);
+  setText(newText.join(" "))
+  props.showAlert("Extra Spaces  Removed !", "Success")
+}
+
+
+  const handelOneCheng = (event) => {
+    setText(event.target.value)
+  }
+
+
+  return (
+    <div className='container'  style={{backgroundColor: props.mode === "light" ? props.mode : "light"}}>
+    
+      <div className='container'>
+        <h2>{props.heading}</h2>
+        <div class="mb-3">
+          <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handelOneCheng} style={{backgroundColor: props.mode ==='light'? props.mode : 'light'}} ></textarea>
+        </div>
+        <button className='btn btn-primary mx-2' onClick={handelUpClick}>Convert to Uppercase</button>
+        <button className='btn btn-primary mx-2' onClick={handelLoClick}>Convert to Lowercase</button>
+        <button className='btn btn-primary mx-2' onClick={handelClearClick}>clear Text</button>
+        <button className='btn btn-primary mx-2' onClick={handelCopy}>Copy Text</button>
+        <button className='btn btn-primary mx-2' onClick={handelExtraSpaces}>Remove Extra Spaces</button>
+
+      </div>
+      <div>
+        <h2>your text summary</h2>
+        <p>{text ? text.trim().split(" ").length : 0} words and {text.length} Characters </p>
+        <p>{0.008 * text.split(" ").length} minutes read</p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text :"Enter Something in the TextBox above to preview it here"}</p>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default TextForm
